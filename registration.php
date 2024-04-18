20821c08450921dd82cd888dd8b177303d819217
Vasilii Burlacu <vasilii@imaginationmedia.com>
2024-04-18 15:50:10 +0300

    Bugfix

--- registration.php
+++ registration.php
@@ -1,11 +1,7 @@
 <?php
-/**
- * Copyright © 2017 EaDesign by Eco Active S.R.L. All rights reserved.
- * See LICENSE for license details.
- */
 
 \Magento\Framework\Component\ComponentRegistrar::register(
     \Magento\Framework\Component\ComponentRegistrar::LANGUAGE,
-    'burlacuweb_ro_ro_language_pack',
+    'burlacuweb_ro_ro',
     __DIR__
 );
